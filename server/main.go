dcb2287c82c1c8160271291889eeed6fe2e62002
HARISHA SM <70417383+HARISHA-code@users.noreply.github.com>
2024-01-05 13:51:00 +0530

    Update main.go

--- server/main.go
+++ server/main.go
@@ -1,1 +1,21 @@
+package main
 
+import (
+	"context"
+	"fmt"
+	"log"
+	"math/rand"
+	"net"
+	"strconv"
+	"sync"
+
+	"github.com/google/uuid"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+	pb "your-github-repo/train-ticket-app/server"
+)
+
+type server struct {
+	mu              sync.Mutex
+	seatAllocations map[string]pb.SeatAllocation
+}
