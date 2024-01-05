92c2faedc2e7988d94a6715e75056c5b3b1c9c8b
HARISHA SM <70417383+HARISHA-code@users.noreply.github.com>
2024-01-05 13:41:23 +0530

    Update main.go

--- client/main.go
+++ client/main.go
@@ -1,1 +1,16 @@
+// client/main.go
+package main
 
+import (
+    "context"
+    "fmt"
+    "log"
+
+    "google.golang.org/grpc"
+    pb "your-github-repo/train-ticket-app/server"
+)
+
+func main() {
+    // Set up gRPC connection
+    conn, err := grpc.Dial("localhost:50051", grpc.WithInsecure())
+    if err != nil
